05b81ee657dae2b0981687f115b8ee636daa8353
Felix Mairamhof <felix.mairamhof@edu.htl-klu.at>
2024-03-12 18:17:34 +0100

    Build Post API

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -18,11 +18,24 @@ app.get("/jokes/:id",(req,res)=>{
   const id = parseInt(req.params.id);
   const foundJoke = jokes.find((joke) => joke.id === id);
   res.json(foundJoke);
-})
+});
 //3. GET a jokes by filtering on the joke type
-
+app.get("/filter",(req,res)=>{
+  const type = req.query.type;
+  const filterJoke = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filterJoke);
+});
 //4. POST a new joke
-
+app.get("/jokes",(req,res)=>{
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
 //5. PUT a joke
 
 //6. PATCH a joke
