5277fe5aecd6b42ea64c172cbc28930827364904
Felix Mairamhof <feberg.k@gmail.com>
2024-03-16 19:34:35 +0100

    Finish Worl Capital Quiz

--- World Capital Quiz/index.js
+++ World Capital Quiz/index.js
@@ -13,11 +13,22 @@ const db = new pg.Client({
   port: 5432,
 });
 
+db.connect();
+
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+db.query("SELECT * FROM capitals", (err,res) =>{
+  if(err){
+    console.error("Error executing query",err.stack);
+  }else{
+    quiz = res.rows;
+  }
+  db.end();
+})
 
 let totalCorrect = 0;
 
