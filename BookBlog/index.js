637853c420eb4330cd287554f7e1f2a7dd914e3b
Felix Mairamhof <feberg.k@gmail.com>
2024-04-15 09:58:48 +0200

    Initialized, Connected DB, Build Create API

---
+++ BookBlog/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "bookBlog",
+  password: "postgres",
+  port: 5432,
+});
+db.connect();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.get("/create",(req,res)=>{
+    res.render("create.ejs");
+});
+app.post("/create", async (req,res)=>{
+    const title = req.body.title;
+    const describtion = req.body.text;
+    const author = req.body.author;
+
+    await db.query("INSERT INTO bookreview (title,describtion,author) VALUES ($1,$2,$3)",[title,describtion,author]);
+
+    res.redirect("/");
+});
+
+app.listen(port, ()=>{
+    console.log("Running on port" + port);
+});
