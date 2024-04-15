f9d2aec590c1e44edd2de7bf2f531c1842b956b8
Felix Mairamhof <feberg.k@gmail.com>
2024-04-15 15:42:08 +0200

    Build the edit api

--- BookBlog/index.js
+++ BookBlog/index.js
@@ -23,12 +23,35 @@ app.get("/",async(req,res)=>{
         data: data.rows,
     });
 });
-app.post("/edit",(req,res)=>{
+app.post("/edit",async(req,res)=>{
+    if(req.body.editItemId){
+        const editId = req.body.editItemId;
+    
+        const data = await db.query("SELECT * FROM bookreview WHERE id = $1",[editId]);
+        
+        res.render("edit.ejs",{
+            data: data.rows,
+        }); 
+    }
+    else if(req.body.title){
+        const title = req.body.title;
+        const describtion = req.body.describtion;
+        const rating = req.body.rating;
+        const time = req.body.time;
+
+        await db.query("UPDATE bookreview SET title = $1, describtion = $2, rating = $3, time = $4 WHERE title = $1 OR describtion =$2 OR rating = $3 OR time = $4",[title,describtion,rating,time]);
+        
+        res.redirect("/");
+    }
+    else{
+        res.redirect("/");
+    }
+
+
+
 
 });
-app.get("/edit",(req,res)=>{
-    res.render("edit.ejs");
-});
+
 
 app.post("/delete",async(req,res)=>{
     const deleteId = req.body.deleteItemId;
