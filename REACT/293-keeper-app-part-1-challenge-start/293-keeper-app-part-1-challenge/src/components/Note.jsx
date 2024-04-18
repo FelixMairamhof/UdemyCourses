3ef8cf1a64f3fa8dc892f1cbb20ba4239ab1e3d9
Felix Mairamhof <feberg.k@gmail.com>
2024-04-18 21:35:56 +0200

    Build Keeper App Part 1

---
+++ REACT/293-keeper-app-part-1-challenge-start/293-keeper-app-part-1-challenge/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note (){
+    return(
+        <div className="note">
+            <h1>Titel</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio illum dicta impedit iure explicabo, sint dignissimos, beatae nihil a iste aut non modi unde necessitatibus quidem repudiandae atque quaerat animi!</p>
+        </div>
+    );
+}
+export default Note;
