64c80218af46377891d623d5d8aa7d5b09063938
Felix Mairamhof <feberg.k@gmail.com>
2024-04-19 15:48:24 +0200

    Finish Theorie React Props

--- REACT/295-react-props-start/295-react-props/src/index.jsx
+++ REACT/295-react-props-start/295-react-props/src/index.jsx
@@ -26,7 +26,12 @@ ReactDOM.render(
         email="b@beyonce.com"
     />
 
-    <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" tel="+987 654 321" email="jack@nowhere.com"/>
+    <Card 
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
+    />
 
     </div>
    ,document.getElementById("root")
